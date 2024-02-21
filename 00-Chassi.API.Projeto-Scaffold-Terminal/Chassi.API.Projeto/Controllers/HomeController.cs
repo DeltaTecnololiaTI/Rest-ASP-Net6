eb142bf222c84d1baf9287032f8311550883aec5
Gabriel Lopez <delta.tecnologia.ti@gmail.com>
2024-02-21 15:50:53 -0300

    Implementado calculadora

---
+++ 00-Chassi.API.Projeto-Scaffold-Terminal/Chassi.API.Projeto/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chassi.API.Projeto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+    }
+}
---
+++ Chassi.API.Projeto/Controllers/CalculadoraController.cs
@@ -0,0 +1,100 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chassi.API.Projeto.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculadoraController : ControllerBase
+    {
+
+        private readonly ILogger<CalculadoraController> _logger;
+
+        public CalculadoraController(ILogger<CalculadoraController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{numero1}/{numero2}")]
+        public IActionResult Sum(string numero1, string numero2)
+        {
+            if (IsNumeric(numero1) && IsNumeric(numero2) )
+            {
+                decimal soma = ConvertDecimal(numero1) + ConvertDecimal(numero2);
+                return Ok(soma.ToString());
+            }
+            return BadRequest("Número Inválido!");
+        }
+        [HttpGet("subtraction/{numero1}/{numero2}")]
+        public IActionResult Subtraction(string numero1, string numero2)
+        {
+            if (IsNumeric(numero1) && IsNumeric(numero2))
+            {
+                decimal subtracao = ConvertDecimal(numero1) - ConvertDecimal(numero2);
+                return Ok(subtracao.ToString());
+            }
+            return BadRequest("Número Inválido!");
+        }
+        [HttpGet("multiplication/{numero1}/{numero2}")]
+        public IActionResult Multiplication(string numero1, string numero2)
+        {
+            if (IsNumeric(numero1) && IsNumeric(numero2))
+            {
+                decimal multiplicacao = ConvertDecimal(numero1) * ConvertDecimal(numero2);
+                return Ok(multiplicacao.ToString());
+            }
+            return BadRequest("Número Inválido!");
+        }
+        [HttpGet("division/{numero1}/{numero2}")]
+        public IActionResult Division(string numero1, string numero2)
+        {
+            if (IsNumeric(numero1) && IsNumeric(numero2))
+            {
+                decimal division = ConvertDecimal(numero1) / ConvertDecimal(numero2);
+                return Ok(division.ToString());
+            }
+            return BadRequest("Número Inválido!");
+        }
+        [HttpGet("mean/{numero1}/{numero2}")]
+        public IActionResult Mean(string numero1, string numero2)
+        {
+            if (IsNumeric(numero1) && IsNumeric(numero2))
+            {
+                decimal media = (ConvertDecimal(numero1) + ConvertDecimal(numero2))/2;
+                return Ok(media.ToString());
+            }
+            return BadRequest("Número Inválido!");
+        }
+        [HttpGet("square-root/{numero1}")]
+        public IActionResult SquareRoot(string numero1)
+        {
+            if (IsNumeric(numero1) )
+            {
+                var squareRoot = Math.Sqrt((double)ConvertDecimal(numero1));
+                return Ok(squareRoot.ToString());
+            }
+            return BadRequest("Número Inválido!");
+        }
+        private bool IsNumeric(string strNumero)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumero
+                , System.Globalization.NumberStyles.Any
+                , System.Globalization.NumberFormatInfo.InvariantInfo
+                , out number);
+            return isNumber;
+
+        }
+        private decimal ConvertDecimal(string strNumero)
+        {
+            decimal decimalNumber;
+            if (decimal.TryParse(strNumero, out decimalNumber))
+            {
+                return decimalNumber;
+            }
+            return 0;
+
+        }
+
+
+    }
+}
